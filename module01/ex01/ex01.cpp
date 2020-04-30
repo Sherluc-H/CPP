d57bbde2515b4f6d35034680e131e277d30da8e2
lhuang <lhuang@student.42.fr>
2020-04-30 22:12:16 +0200

    module01 ex01 add newline

--- module01/ex01/ex01.cpp
+++ module01/ex01/ex01.cpp
@@ -6,7 +6,7 @@
 /*   By: lhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 14:19:03 by lhuang            #+#    #+#             */
-/*   Updated: 2020/04/30 21:13:05 by lhuang           ###   ########.fr       */
+/*   Updated: 2020/04/30 22:02:02 by lhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void	memoryLeak()
 {
 	std::string*	panther = new std::string("String panther");
-	std::cout << *panther << std::endl;
 
+	std::cout << *panther << std::endl;
 	delete panther;
 }
